954f334aaea9f31898261a5f02a958c16c66963d
Hajsori <hajsori@proton.me>
2026-03-09 14:02:17 +0100

    Random Potion Effects

--- src/main/kotlin/xyz/hajsori/pillars/Pillars.kt
+++ src/main/kotlin/xyz/hajsori/pillars/Pillars.kt
@@ -1,9 +1,14 @@
 package xyz.hajsori.pillars
 
 import org.bukkit.Material
+import org.bukkit.Registry
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.PotionMeta
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 import xyz.hajsori.pillars.events.ChatListener
+import kotlin.reflect.typeOf
 
 
 class Pillars : JavaPlugin() {
@@ -30,9 +35,20 @@ class Pillars : JavaPlugin() {
 
         scheduler.runTaskTimer(this, { task ->
             for (player in this.server.worlds[0].players) {
-                player.give(listOf(ItemStack(Material.entries.filter { material ->
+                val randomItem = ItemStack(Material.entries.filter { material ->
                     material.isItem && !forbiddenItems.contains(material)
-                }.random())), false)
+                }.random())
+                if (randomItem == ItemStack(Material.POTION) || randomItem == ItemStack(Material.SPLASH_POTION) || randomItem == ItemStack(Material.LINGERING_POTION) ) {
+                    val randomEffect = Registry.POTION.keyStream().toList().random()
+
+                    if (randomItem.itemMeta is PotionMeta) {
+                        val meta = randomItem.itemMeta as PotionMeta
+                        meta.basePotionType = Registry.POTION.get(randomEffect)
+                        randomItem.itemMeta = meta
+                    }
+                }
+
+                player.give(listOf(randomItem), false)
             }
         }, 0, 60)
     }
